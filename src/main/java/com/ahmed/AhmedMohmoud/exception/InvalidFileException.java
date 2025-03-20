55f9e830636015ed7cb866a61652366b15c77cf5
ahmeed27 <ahmdbrl811@gmail.com>
2025-03-20 14:29:46 +0200

    exceptions package

---
+++ src/main/java/com/ahmed/AhmedMohmoud/exception/InvalidFileException.java
@@ -0,0 +1,7 @@
+package com.ahmed.AhmedMohmoud.exception;
+
+public class InvalidFileException extends RuntimeException {
+    public InvalidFileException(String message) {
+        super(message);
+    }
+}
