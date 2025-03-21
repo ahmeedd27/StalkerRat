99f53a9c893a60f5911f084a81ded67c6fe1be2d
ahmeed27 <ahmdbrl811@gmail.com>
2025-03-21 09:29:51 +0200

    update to use clodniary

--- src/main/java/com/ahmed/AhmedMohmoud/security/SecurityConfig.java
+++ src/main/java/com/ahmed/AhmedMohmoud/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("https://crucial-michaelina-personal27-17f4cba2.koyeb.app" , "localhost:8080"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS" ,"PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
